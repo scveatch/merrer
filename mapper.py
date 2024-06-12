a49572d1bb40f36056690787133320b0a1842a86
scveatch <124098682+scveatch@users.noreply.github.com>
2024-06-11 20:34:40 -0700

    Update mapper.py
    Add functional code

--- mapper.py
+++ mapper.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
 import sys 
-  
-for line in sys.stdin: 
-    line = line.strip() 
-    words = line.split() 
-    for word in words:
+
+def process(line):
+    word = ""
+
+    for char in line:
+        if char.isalpha():
+            word += char
+        else:
+            if word != "":
+                print('%s\t%s' % (word,1))
+                word = ""
+
+    if word != "":
         print('%s\t%s' % (word, 1))
+
+for line in sys.stdin:
+    line = line.strip().lower()
+    process(line)
